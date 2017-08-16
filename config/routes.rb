b099c9c4ac645f5848a6ff78fbe5a1efc8b6d199
Yoon Jee Woo <yoonjeewoo@gmail.com>
2017-08-16 21:16:17 +0900

    학교 선택

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
 end
   get 'home/index'
   get 'home/school'
+  get '/time' => 'time#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
