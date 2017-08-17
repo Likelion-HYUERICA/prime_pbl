a5b51cf7f889ce610c8764dddc32ea207f9bd2f9
KyeongDeok <kdpark1623@gmail.com>
2017-08-17 00:22:08 +0000

    로그인 필터(hanyang.ac.kr)추가

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,:confirmable
+         
+  validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@hanyang.ac.kr\z/, message: "must be a hanyang.ac.kr account" }
 end
