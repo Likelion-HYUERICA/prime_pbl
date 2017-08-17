94225ae2588c6d881b7fbb05552764a13159c2f3
Yoon Jee Woo <yoonjeewoo@gmail.com>
2017-08-17 17:18:02 +0900

    이용자추가 페이지

--- app/controllers/time_controller.rb
+++ app/controllers/time_controller.rb
@@ -1,5 +1,6 @@
 class TimeController < ApplicationController
+	# 방시간선택
 	def index
-		
+
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'room/index'
+
   devise_for :users
    devise_scope :user do
     authenticated :user do
@@ -11,6 +13,7 @@ Rails.application.routes.draw do
 end
   get 'home/index'
   get 'home/school'
-  get '/time' => 'time#index'
+  get 'time/index'
+  get 'room/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
